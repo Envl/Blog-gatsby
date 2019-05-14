d04a856956d138190e1100db90c3aba81e4f30af
Envl <yyaomingm@gmail.com>
2019-05-14 23:45:14 +0200

    environment test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <SEO title="Sesame Yard" keywords={[`UX`, `Design`, "web", `react`]} />
+    <h1>Hi people{process.env.NODE_ENV}</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
